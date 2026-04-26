fbd450a39a3063f295fd75ab94430b53d80bc4f3
Victor Queiroz <victorqs1997@hotmail.com>
2026-04-26 15:07:57 -0300

    Add SplashScreen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.core.splashscreen)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
--- app/src/main/java/com/slicedwork/slicedwork/MainActivity.kt
+++ app/src/main/java/com/slicedwork/slicedwork/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.slicedwork.designsystem.theme.SlicedWorkTheme
 import com.slicedwork.slicedwork.ui.SlicedWorkApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
